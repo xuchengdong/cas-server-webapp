40d8e5c8dace9c71fad9a62cc4d2d143761643c8
xuchengdongxcd@126.com <xwx180487>
2016-11-23 11:25:27 +0800

    修改Linux环境下一键打包发布脚本

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
-cd ~/Workspace/IdeaProjects/cas-server-webapp
+export CATALINA_BASE=/home/xucd/Workspace/webserver/tomcat-sso/tomcat-cas
+echo $CATALINA_BASE
+echo ========开始编译打包===========
 mvn clean package
+echo ========打包完成==============
+echo ========杀掉进程==============
 ps -ef |grep tomcat-cas |awk '{print $2}'|xargs kill -9
-rm -rf ~/Workspace/webserver/tomcat-sso/tomcat-cas/webapps/cas*
-cp ./target/cas.war ~/Workspace/webserver/tomcat-sso/tomcat-cas/webapps
-cd ~/Workspace/webserver/tomcat-sso/tomcat-cas
-./bin/startup.sh | tail -f ./logs/catalina.out
+#kill -9 `ps -ef | grep tomcat-cas | tr -s ' ' | awk -F' ' '{print $2}'`
+echo ========清除旧文件==============
+rm -rf $CATALINA_BASE/webapps/cas*
+echo ========复制新文件==============
+cp ./target/cas.war $CATALINA_BASE/webapps
+cd $CATALINA_BASE
+echo ========启动tomcat==============
+./bin/startup.sh | tail -f -n -250 ./logs/catalina.out
 
